25fcf650fa80783caa625d0545f29fb9481387ae
Brontoz554 <tifilum74@mail.ru>
2019-12-15 13:47:25 +0300

    its work

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
 let host = 'http://noroff.beget.tech/api/';
 
-let f = async(url, method = 'get', data = null, useToken = true, formdata = false) =>{
+let f = async (url, method = 'get', data = null, useToken = true, formdata = false) => {
     method = method.toUpperCase();
 
-    let options = {method:method, headers : {}};
+    let options = {method: method, headers: {}};
 
-    if(useToken){
-        options.headers['Authorization'] = `Bearer ${app.auth_user.token}`;
+    if (useToken) {
+        options.headers['Authorization'] = `Bearer ${app.user.token}`;
     }
-    if(formdata){
+    if (formdata) {
         var formData = new FormData();
-        for (var name in data){
-            formData.append(name, data[name] );
+        for (var name in data) {
+            formData.append(name, data[name]);
         }
         options.body = formData;
     } else {
-        if(['POST', 'PUT', 'PATCH'].includes(method)){
-            options.body = JSON.stringify(data);
+        if (['POST', 'PUT', 'PATCH'].includes(method)) {
+            if (data) {
+                options.body = JSON.stringify(data);
+            }
         }
     }
 
     const res = await fetch(host + url, options);
 
-    if(method == 'DELETE') {
+    if (method == 'DELETE') {
         return true;
     } else {
         return await res.json();
@@ -32,7 +34,7 @@ let f = async(url, method = 'get', data = null, useToken = true, formdata = fals
 let app = new Vue({
     el: '#app',
     data: {
-        window: 'registerVolunteer',
+        window: 'registerOVZ',
         home: '',
         edit: '',
         activeQuests: '',
@@ -43,43 +45,104 @@ let app = new Vue({
             password: '',
             phone: '',
             address: '',
+            additional_phone: '',
+            invalid_certificate: [],
+            disease: '',
             photo_user: [],
             criminal_record: [],
             drug_certificate: [],
             passport: [],
             self_passport: [],
+            token: null,
             coins: '',
+
+
         },
-        cities:[],
-        errors:[],
+        cities: [],
+        allDisease: [],
+        errors: [],
+        loginErrors: [],
+        tasks: {},
+        dis: {},
     },
     methods: {
-        async Auth() { //Авторизация
-            let result = await f('v_signup', 'post', this.user, false,true);
-            if(result.id){
-                this.window = 'main';
+        async SignUp() {
+            let result = await f('v_signup', 'post', this.user, false, true);
+            if (result.id) {
+                this.window = 'auth';
             } else {
                 this.errors = result;
             }
         },
-        uploadPassport(event){
+
+        async SignUpOVZ() {
+            let result = await f('ovz_signup', 'post', this.user, false, true);
+            if (result.id) {
+                this.window = 'auth';
+            } else {
+                this.errors = result;
+            }
+        },
+
+        async Login() {
+            let result = await f('signin', 'post', this.user, false, true);
+            if (result.token) {
+                this.user.token = result.token;
+                localStorage.setItem('token',this.user.token);
+                this.window = 'main';
+                this.getAllTasks();
+            } else {
+                this.loginErrors = result;
+            }
+        },
+
+        async logout() {
+            let result = await f('logout', 'post', null, true, false);
+            localStorage.removeItem('token');
+            this.user.token = null;
+            this.window = 'auth';
+        },
+
+
+        async getCities() {
+
+            let res = await f('cities', 'get', null, false);
+            this.cities = res;
+        },
+
+        async getAllDisease() {
+            this.allDisease = await f('disease', 'get', null, false);
+
+        },
+
+        async getAllTasks() {
+            this.tasks = await f('tasks', 'get', null, true);
+            console.log(this.tasks);
+
+        },
+
+        uploadPassport(event) {
             this.user.passport = event.target.files[0];
         },
-        uploadCriminalRecord(event){
+        uploadCriminalRecord(event) {
             this.user.criminal_record = event.target.files[0];
         },
 
-        uploadDrugCertificate(event){
+        uploadDrugCertificate(event) {
             this.user.drug_certificate = event.target.files[0];
         },
 
-        uploadSelfPassport(event){
+        uploadSelfPassport(event) {
             this.user.self_passport = event.target.files[0];
         },
 
-        uploadPhotoUser(event){
+        uploadPhotoUser(event) {
             this.user.photo_user = event.target.files[0];
 
+        },
+
+        uploadInvalidCertificate(event) {
+            this.user.invalid_certificate = event.target.files[0];
         },
 
         setEdit() {
@@ -89,11 +152,7 @@ let app = new Vue({
             this.page = 'Edit';
         },
 
-        async getCities(){
 
-            let res = await f('cities','get',null,false);
-            this.cities = res;
-        },
         setHome() {
             this.home = 'Главная';
             this.edit = '';
@@ -107,11 +166,16 @@ let app = new Vue({
             this.home = '';
             this.page = 'ActiveQuests';
         },
-        logout() {
-            this.window = 'auth';
-        },
 
-
+        async getAll(){
+            this.user.token = localStorage.getItem('token') || null;
+            if(this.user.token){
+                this.window = 'main';
+            }
+            this.getAllTasks();
+            this.getAllDisease();
+            this.getCities();
+        }
     },
 });
-app.getCities();
+app.getAll();
